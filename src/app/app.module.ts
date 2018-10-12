57bd9eb4ed44288b6134a91fcc2ca8201e2f571b
gmilkov <g.milkov.milkov@gmail.com>
2018-10-12 11:30:08 +0300

    sg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { routerConfig } from './routes/top-menu.config';
+import { HomeComponent } from './components/home.component';
+import {DestinationsComponent} from './components/destinations.component';
+import { PhotosComponent } from './components/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DestinationsComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
